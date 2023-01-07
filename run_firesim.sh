ad0d7117e133129b889f06f98b516690eb45d3b0
zhejiang.uk@gmail.com <centos@ip-192-168-2-120.eu-west-1.compute.internal>
2023-01-07 01:35:36 +0000

    Added a script to run firesim.

---
+++ run_firesim.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Input flags
+gc_kernel=none
+
+while getopts k: flag
+do
+	case "${flag}" in
+		k) gc_kernel=${OPTARG};;
+	esac
+done
+
+input_type=simmedium
+path_firesim=/home/centos/firesim
+path_parsec=/home/centos/parsec_gc
+
+path_firesim_sw=${path_firesim}/sw/firesim-software/
+path_firesim_sw_workloads=${path_firesim_sw}/gc-workloads/gc-fed/overlay/root
+path_firesim_workloads=${path_firesim}/deploy/workloads/gc-fed
+
+path_parsec_pkgs=${path_parsec}/pkgs
+
+cd ${path_firesim_sw_workloads}
+if [ -r "pkgs" ]; then
+    cmd="rm -rf ${path_firesim_sw_workloads}/pkgs"
+    echo "${cmd}"
+    eval ${cmd}
+fi
+
+cd ${path_firesim_workloads}
+cmd="rm -rf ${path_firesim_workloads}/*"
+echo "${cmd}"
+eval ${cmd}
+
+cd ${path_parsec_pkgs}
+cmd="./build_parsec.sh -k ${gc_kernel}"
+echo "${cmd}"
+eval ${cmd}
+
+cd ${path_parsec}
+cmd="cp -rf pkgs ${path_firesim_sw_workloads}"
+echo "${cmd}"
+eval ${cmd}
+
+cd ${path_firesim_sw}
+cmd="./marshal clean gc-workloads/gc-fed.json"
+echo "${cmd}"
+eval ${cmd}
+
+cmd="./marshal build gc-workloads/gc-fed.json"
+echo "${cmd}"
+eval ${cmd}
+
+cd images
+cmd="cp -rf gc-fed-bin gc-fed.img ${path_firesim_workloads}"
+echo "${cmd}"
+eval ${cmd}
+
+cmd="firesim launchrunfarm && firesim infrasetup && firesim runworkload"
+echo "${cmd}"
+eval ${cmd}
