105d8e49ec1a6ce8ead6c0d09b65a37408a0a1a6
zhejianguk <97304283+zhejianguk@users.noreply.github.com>
2025-07-15 01:55:30 +0800

    Update build_parsec.sh

--- pkgs/build_parsec.sh
+++ pkgs/build_parsec.sh
@@ -5,29 +5,11 @@ gc_kernel=none
 rm -f *.o
 rm -f *.riscv
 
-# Input flags
-while getopts k: flag
-do
-	case "${flag}" in
-		k) gc_kernel=${OPTARG};;
-	esac
-done
-
 input_type=simmedium
-
-export PATH_GC_KERNELS="/home/centos/r-kernel_firesim/"
-export PATH_PKGS=$PWD
-
-cd $PATH_GC_KERNELS
-make clean
-make main_${gc_kernel}
-
-make initialisation_${gc_kernel}
-cp initialisation_${gc_kernel}.riscv $PATH_PKGS
 
 
 # BENCHMARKS=(blackscholes bodytrack swaptions x264)
-BENCHMARKS=(dedup)
+BENCHMARKS=(blackscholes bodytrack dedup facesim ferret fluidanimate freqmine streamcluster swaptions x264)
 
 cmd="parsecmgmt -a clean -p all"
 eval ${cmd}
